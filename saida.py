54682de87c30323bb689114ff31718f7dc155de6
Voyane Rogerio <voyanerogerio@hotmail.com>
2024-12-13 15:12:29 +0200

    Criacao do ficheiro python Saida,observando alguns exemplos com f-string.

---
+++ saida.py
@@ -0,0 +1,6 @@
+print(f"O numero inteiro binario e': {10: b}")
+print(f"O numero inteiro decimal e': {10: d}")
+
+import math
+print(f"O valor de PI e': {math.pi}")
+print(f"O valor de PI e': {math.pi: .2f}")
