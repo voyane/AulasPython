2058fba15bc21e6f3c8bf7a169352c279a741175
Voyane Rogerio <voyanerogerio@hotmail.com>
2024-12-13 16:03:09 +0200

    Criacao

---
+++ desafioentrada.py
@@ -0,0 +1,6 @@
+celular = input("Digite o seu numero de celular: ")
+print(celular)
+bilhete = input("Digite o seu numero de bilhete de identidade: ")
+print(bilhete)
+
+print(f"O numero de celular e: ", celular, " e o numero do bilhete de identidade e': ", bilhete)
--- entrada.py
+++ entrada.py
@@ -1,1 +1,4 @@
 print("O numero digitedo foi: ", input("Digite um numero: "))
+
+dado = input("Digite a idade: ")
+print("A idade digitada e': ", dado)
