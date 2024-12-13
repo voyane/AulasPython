f470107bf26fde528496d4a9e317f01b2a8858c3
Voyane Rogerio <voyanerogerio@hotmail.com>
2024-12-13 16:48:14 +0200

    O ficheiro variaveis foi modificado.

---
+++ variaveis.py
@@ -0,0 +1,5 @@
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+altura = float(input("Digite a sua altura: "))
+
+print(f"o seu nome e' ", nome, ", voce tem: ", idade, "anos de idade e mede ", altura, "m de altura.")
