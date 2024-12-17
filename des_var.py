eb6c83277dc11e83171be364456642dc0aef0995
Voyane Rogerio <voyanerogerio@hotmail.com>
2024-12-17 15:21:19 +0200

    Criacao

---
+++ des_var.py
@@ -0,0 +1,8 @@
+id = int(input("Digite o seu ID: "))
+nome = str(input("Digite o seu nome: "))
+salario = float(input("Digite o seu salario: "))
+moz = True
+
+print(f"O seu ID e'", id, ", seu nome e' ", nome, ", voce recebe ", salario, " e voce e' mocambicano ", moz)
+
+
